ebd3a150805527c984f740949af4f8a4ccbd9504
ArLazy <160593042+ArLazy@users.noreply.github.com>
2026-01-30 15:59:31 +0500

    2 практическая формы
    Приложение WinForm по второй практической

---
+++ 2практическаяФормы/prakt2ZverevaWinForm/prakt2ZverevaWinForm/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace prakt2ZverevaWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxBase1 = new System.Windows.Forms.TextBox();
+            this.textBoxSide = new System.Windows.Forms.TextBox();
+            this.textBoxHeight = new System.Windows.Forms.TextBox();
+            this.textBoxBase2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxArea = new System.Windows.Forms.TextBox();
+            this.textBoxPerimeter = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxBase1
+            // 
+            this.textBoxBase1.Location = new System.Drawing.Point(112, 44);
+            this.textBoxBase1.Name = "textBoxBase1";
+            this.textBoxBase1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxBase1.TabIndex = 0;
+            // 
+            // textBoxSide
+            // 
+            this.textBoxSide.Location = new System.Drawing.Point(350, 87);
+            this.textBoxSide.Name = "textBoxSide";
+            this.textBoxSide.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSide.TabIndex = 1;
+            // 
+            // textBoxHeight
+            // 
+            this.textBoxHeight.Location = new System.Drawing.Point(350, 44);
+            this.textBoxHeight.Name = "textBoxHeight";
+            this.textBoxHeight.Size = new System.Drawing.Size(100, 20);
+            this.textBoxHeight.TabIndex = 2;
+            // 
+            // textBoxBase2
+            // 
+            this.textBoxBase2.Location = new System.Drawing.Point(112, 87);
+            this.textBoxBase2.Name = "textBoxBase2";
+            this.textBoxBase2.Size = new System.Drawing.Size(100, 20);
+            this.textBoxBase2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Основание 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(247, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Боковая сторона:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Основание 2:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(296, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Высота:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(112, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 235);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Площадь:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(38, 269);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Периметр:";
+            // 
+            // textBoxArea
+            // 
+            this.textBoxArea.Location = new System.Drawing.Point(101, 232);
+            this.textBoxArea.Name = "textBoxArea";
+            this.textBoxArea.ReadOnly = true;
+            this.textBoxArea.Size = new System.Drawing.Size(100, 20);
+            this.textBoxArea.TabIndex = 11;
+            // 
+            // textBoxPerimeter
+            // 
+            this.textBoxPerimeter.Location = new System.Drawing.Point(101, 269);
+            this.textBoxPerimeter.Name = "textBoxPerimeter";
+            this.textBoxPerimeter.ReadOnly = true;
+            this.textBoxPerimeter.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPerimeter.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxPerimeter);
+            this.Controls.Add(this.textBoxArea);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxBase2);
+            this.Controls.Add(this.textBoxHeight);
+            this.Controls.Add(this.textBoxSide);
+            this.Controls.Add(this.textBoxBase1);
+            this.Name = "Form1";
+            this.Text = "Здравствуйте, 2 практическая вычисляем трапецию";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxBase1;
+        private System.Windows.Forms.TextBox textBoxSide;
+        private System.Windows.Forms.TextBox textBoxHeight;
+        private System.Windows.Forms.TextBox textBoxBase2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxArea;
+        private System.Windows.Forms.TextBox textBoxPerimeter;
+    }
+}
+
---
+++ 2практическаяФормы/prakt2ZverevaWinForm/prakt2ZverevaWinForm/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prakt2ZverevaWinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            double base1, base2, height, side, area, perimeter;
+
+            
+            base1 = Convert.ToDouble(textBoxBase1.Text);
+            base2 = Convert.ToDouble(textBoxBase2.Text);
+            height = Convert.ToDouble(textBoxHeight.Text);
+            side = Convert.ToDouble(textBoxSide.Text);
+
+           
+            area = ((base1 + base2) / 2) * height;
+
+            
+            perimeter = base1 + base2 + height + side;
+
+           
+            textBoxArea.Text = area.ToString("F2");
+            textBoxPerimeter.Text = perimeter.ToString("F2");
+        }
+    }
+}
